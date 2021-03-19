82e96ddd65eec10e590d3a9e95f742bfbf45342b
Krish Dev DB <krishdevdb@gmail.com>
2021-03-19 21:48:41 +0530

    Add Click Feature

--- drums/scripts/script.js
+++ drums/scripts/script.js
@@ -1,14 +1,33 @@
+// Click Method
+document.querySelectorAll('div[data-letter]').forEach((key) => {
+  key.addEventListener('click', () => {
+    const audio = document.querySelector(`audio[data-letter=${key.getAttribute('data-letter')}]`)
+
+    if (!audio) return
+
+    key.classList.add('playing')
+
+    audio.currentTime = 0
+    audio.play()
+
+    setTimeout(() => {
+      key.classList.remove('playing')
+    }, (audio.duration * 100))
+  })
+})
+
+// Keyboard Method
 window.addEventListener('keydown', (ev) => {
-  const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${ev.keyCode}"]`);
-  if (!audio) return;
+  const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`)
+  const key = document.querySelector(`div[data-key="${ev.keyCode}"]`)
+  if (!audio) return
 
-  key.classList.add('playing');
+  key.classList.add('playing')
 
-  audio.currentTime = 0;
-  audio.play();
+  audio.currentTime = 0
+  audio.play()
 
   setTimeout(() => {
-    key.classList.remove('playing');
-  }, (audio.duration * 100));
-});
+    key.classList.remove('playing')
+  }, (audio.duration * 150))
+})
