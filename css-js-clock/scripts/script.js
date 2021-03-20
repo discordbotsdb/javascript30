bfd5cb4f47c3cf7e5c982d569143bf4aa228235b
Krish Dev DB <krishdevdb@gmail.com>
2021-03-20 21:35:19 +0530

    Fix Minute Hand

--- css-js-clock/scripts/script.js
+++ css-js-clock/scripts/script.js
@@ -5,9 +5,9 @@ const hours = document.getElementById('hours')
 setInterval(() => {
   const time = new Date()
 
-  seconds.style.transform = `rotate(${((time.getSeconds() / 60) * 360) + 90}deg)`
+  seconds.style.transform = `rotate(${(((time.getSeconds() / 60) * 360) + 90)}deg)`
 
-  minutes.style.transform = `rotate(${((time.getMinutes() / 60) * 360) + ((seconds / 60) * 6) + 90}deg)`
+  minutes.style.transform = `rotate(${(((time.getMinutes() / 60) * 360) + ((time.getSeconds() / 60) * 6) + 90)}deg)`
 
-  hours.style.transform = `rotate(${((time.getHours() / 12) * 360) + ((time.getMinutes() / 60) * 30) + 90}deg)`
-}, 1000)
+  hours.style.transform = `rotate(${(((time.getHours() / 12) * 360) + ((time.getMinutes() / 60) * 30) + 90)}deg)`
+}, 100)
